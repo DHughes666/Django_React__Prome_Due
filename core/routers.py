c637a21e8d7ce9d0ca98d190ccc27835789a1ace
DHughes666 <graziemolto6@gmail.com>
2023-11-10 22:01:02 +0100

    Added  logic to the login feature

--- core/routers.py
+++ core/routers.py
@@ -2,6 +2,7 @@ from rest_framework import routers
 from core.user.viewsets import UserViewSet
 from core.auth.viewsets.register import RegisterViewSet
 from core.auth.viewsets.login import LoginViewSet
+from core.auth.viewsets.refresh import RefreshViewSet
 
 router = routers.SimpleRouter()
 
@@ -10,6 +11,7 @@ router.register(r"user", UserViewSet, basename='user')
 #AUTH
 router.register(r"auth/register", RegisterViewSet, basename='auth-register')
 router.register(r"auth/login", LoginViewSet, basename='auth-login')
+router.register(r"auth/refresh", RefreshViewSet, basename='auth-refresh')
 
 
 urlpatterns = [
