5b79e1029933f6018a5e3b3729292a6cfe95430b
DHughes666 <graziemolto6@gmail.com>
2023-11-11 06:28:47 +0100

    Post serialized and processed into viewsets, routers all set too

--- core/routers.py
+++ core/routers.py
@@ -3,6 +3,7 @@ from core.user.viewsets import UserViewSet
 from core.auth.viewsets.register import RegisterViewSet
 from core.auth.viewsets.login import LoginViewSet
 from core.auth.viewsets.refresh import RefreshViewSet
+from core.post.viewsets import PostViewSet
 
 router = routers.SimpleRouter()
 
@@ -12,6 +13,9 @@ router.register(r"user", UserViewSet, basename='user')
 router.register(r"auth/register", RegisterViewSet, basename='auth-register')
 router.register(r"auth/login", LoginViewSet, basename='auth-login')
 router.register(r"auth/refresh", RefreshViewSet, basename='auth-refresh')
+
+#POST
+router.register(r"post", PostViewSet, basename='post')
 
 
 urlpatterns = [
