40f3169c4202650d19596f7589974d423356d52c
DHughes666 <graziemolto6@gmail.com>
2023-11-16 20:05:56 +0100

    Registration and Login features ...frontend... implemented

--- postgram-app/src/App.js
+++ postgram-app/src/App.js
@@ -2,6 +2,7 @@ import {Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import ProtectedRoute from './routes/ProtectedRoute';
 import Registration from './pages/Registration';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -13,11 +14,7 @@ function App() {
           </ProtectedRoute>
         }
       />
-      <Route path='/login/' element={
-        <div>Login</div>
-      } 
-
-      />
+      <Route path='/login/' element={<Login />} />
       <Route path='/register/' element={<Registration />} />
     </Routes>
   );
