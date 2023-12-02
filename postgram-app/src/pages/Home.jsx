5e553944537f82bf8968d58d2e27459ee8b38170
DHughes666 <graziemolto6@gmail.com>
2023-12-02 22:19:17 +0100

    Front-end... Post features ...In progress

--- postgram-app/src/pages/Home.jsx
+++ postgram-app/src/pages/Home.jsx
@@ -1,8 +1,39 @@
 import Layout from "../components/Layout";
+import {Row, Col, Image} from "react-bootstrap";
+import {randomAvatar} from "../utils";
+import useSWR from "swr"
+import {fetcher} from "../helpers/axios";
+import { getUser } from "../hooks/user_actions";
+import CreatePost from "../components/posts/CreatePost";
 
 const Home = () => {
+    const user = getUser();
+
+    if(!user) {
+        return <div>Loading!</div>
+    }
+
     return (
-        <Layout />
+        <Layout>
+            <Row className="justify-content-evenly">
+                <Col sm={7}>
+                    <Row className="border rounded align-items-center">
+                        <Col className="flex-shrink-1">
+                            <Image 
+                                src={randomAvatar()}
+                                roundedCircle
+                                width={52}
+                                height={52}
+                                className="my-2"
+                            />
+                        </Col>
+                        <Col sm={10} className="flex-grow-1">
+                            <CreatePost />
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </Layout>
     )
 }
 
